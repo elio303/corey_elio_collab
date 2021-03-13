09832457d320766d65629ff88ebc91bf46fec6e3
taeyangcode <byetaeyang@gmail.com>
2021-03-13 09:59:17 -0800

    Made routes and controllers (articles)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :user, only: [:index, :show]
   get 'user/index'
+  get "/articles", to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'user#index'
