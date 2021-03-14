ef0c0c4316ffd6f0b32a13b02604f0e5267e134c
taeyangcode <byetaeyang@gmail.com>
2021-03-13 22:45:34 -0800

    Deleted 'user' route and mapped the root path to 'articles'
    Defined and generated a model 'Article'
    
    Created table and initialized new 'Article' object
    
    Edited index.html.erb file to display all article titles within the database
    
    Made method and view to display individual articles using an id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :user, only: [:index, :show]
-  get 'user/index'
-  get "/articles", to: "articles#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index"
 
-  root 'user#index'
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
